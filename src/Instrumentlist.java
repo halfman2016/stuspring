9a43e7c1151338aed97853cb26146c210c116b89
jiangfeng <halfman@21cn.com>
2019-04-01 10:20:35 +0800

    instrument

--- src/Instrumentlist.java
+++ src/Instrumentlist.java
@@ -1,11 +1,25 @@
 public class Instrumentlist implements Performer {
     private String song;
+    private Instrument instrument;
 
     public   Instrumentlist(){
     }
     public void  perform() throws PerformanceException{
         System.out.print("Playing" + song + ":");
+instrument.play();
+    }
 
+    public  void setSong(String song){
+        this.song=song;
+    }
+    public String getSong(){
+        return song;
+    }
+    public String screamSong(){
+        return song;
+    }
+    public void setInstrument(Instrument instrument){
+        this.instrument=instrument;
     }
 }
 
